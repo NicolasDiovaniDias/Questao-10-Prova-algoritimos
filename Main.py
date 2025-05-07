c76ce51726364107cbcccc23a1f40272954315eb
Nicolas Diovani <nicolasdiovanioliveira@gmail.com>
2025-05-07 10:33:42 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,5 +2,5 @@ from Aluno import Aluno
 pessoa1 = Aluno(1, "nicolas", 19029409021, "12345678910")
 pessoa1.marcarPresenca()
 pessoa1.imprimir()
-pessoa1.set__matricula(1220304003)
+pessoa1.set__matricula(3020304003)
 print(pessoa1.get__matricula())
